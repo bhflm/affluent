755d6674e267d738521c3a699aa3e3d3135123fd
Germi <germanbonin@gmail.com>
2020-05-21 16:46:13 -0300

    Remove: some comment

--- app/db/users.js
+++ app/db/users.js
@@ -15,21 +15,6 @@ exports.createTable = async db => {
   }
 };
 
-// !! check comment that bulk insert if not working bc of mysql version @ the host :thinking;
-// exports.bulkInsert = async (db, params) => {
-//   logger.info('Bulk users insert');
-//   const statement = 'INSERT INTO `users` (`email` ,`first_name`, `last_name`, `avatar`) VALUES ()';
-//   try {
-//     const [rows, fields] = await db.execute(statement, params);
-//     logger.info('Bulk insert success');
-//     return {};
-//   }
-//   catch(error) {
-//     logger.error(`Error executing statement within users table ${error}`);
-//     return Promise.reject(error);
-//   }
-// };
-
 exports.insertOne = async (db, params) => {
   logger.info('Inserting one user');
   const statement = 'INSERT INTO `users` (`email`, `last_name`, `first_name`, `avatar`) VALUES (?, ?, ?, ?)';
